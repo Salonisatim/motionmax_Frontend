664dbf6bdd9c68f663a1caac96b5f31a807a2b8a
Saloni.Satim <salonisatim6@gmail.com>
2026-04-01 11:09:59 +0530

    Update Courses.js

--- Courses.js
+++ Courses.js
@@ -5,56 +5,56 @@
                 software: "Photoshop, Illustrator, CorelDRAW, InDesign, Canva (Bonus).",
                 desc: "Turn ideas into impactful visuals. This course equips you with the principles of design and the tools to create logos, brand identities, marketing materials, and digital creatives that communicate effectively.", 
                 careers: ["Graphic Designer","Brand Designer","Digital Artist","Creative Executive"],
-               img :"img/Graphic Design.jpg.jpeg " 
+               img :"https://ik.imagekit.io/motionmax25/courses/Graphic%20Design.jpg.jpeg?updatedAt=1773762683988" 
             },
             { 
                 title: "Motion Graphic", cat: "designing", duration: " 6–12 Months", mode: "Offline / Online",
                 software: "After Effects, Premiere Pro, Photoshop, Illustrator, Audition.",
                 desc: "Create animations and videos that tell powerful stories. Learn how to bring still images to life and edit dynamic video content for advertising, entertainment, and digital platforms", 
                 careers: ["Motion Graphics Artist","Video Editor","Social Media Content Creator","Ad Film Designer"],
-                img: "img/motion graphic.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/motion%20graphic.jpg.jpeg?updatedAt=1773762683946" 
             },
             { 
                 title: "Video Editing", cat: "designing", duration: " 6–12 Months", mode: "Offline / Online",
                 software: "After Effects, Premiere Pro, Photoshop, Illustrator, Audition.",
                 desc: "Create animations and videos that tell powerful stories. Learn how to bring still images to life and edit dynamic video content for advertising, entertainment, and digital platforms", 
                 careers: ["Motion Graphics Artist","Video Editor","Social Media Content Creator","Ad Film Designer"],
-                img: "img/video editing.webp" 
+                img: "https://ik.imagekit.io/motionmax25/courses/video%20editing.webp?updatedAt=1773762684003" 
             },
             { 
                 title: "Visual Effects (VFX)", cat: "designing", duration: " 12–18 Months", mode: "Offline / Online",
                 software: "Nuke, After Effects, Fusion, Mocha Pro, Photoshop.",
                 desc: "Learn how blockbuster movies and ads create stunning visual effects. This program teaches you the techniques used in professional studios to merge reality with imagination.", 
                 careers: ["VFX Artist","Compositor","Post-Production Specialist","FX Technical Artist"],
-                img: "img/VFX.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/VFX.jpg.jpeg?updatedAt=1773762683983" 
             },
             { 
                 title: "2D Animation", cat: "designing", duration: "4–6 Months ", mode: "Offline / Online",
                 software: "Adobe Animate, After Effects (Basics), Photoshop / Illustrator (for assets).",
                 desc: "Learn the fundamentals of animation and create engaging 2D animated videos, characters, and motion graphics used in advertising, social media, education, and entertainment.", 
                 careers: ["2D Animator","Motion Graphics Designer"," Video Editor","Multimedia Designer","Freelance Animator"],
-                img: "img/Adobe Premier Pro.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/Adobe%20Premier%20Pro.jpg.jpeg?updatedAt=1773762683970" 
             },
             { 
                 title: "3D Animation", cat: "designing", duration: " 12–18 Months", mode: "Offline / Online",
                 software: "Maya, Blender, Cinema 4D, ZBrush, Substance Painter.",
                 desc: "Step into the immersive world of 3D animation, modeling, and storytelling. This course takes you from basic 3D tools to creating professional-quality animation for films, games, and commercials.", 
                 careers: ["3D Animator","Game Designer","Character Modeler","3D Environment Artist"],
-                img: "img/3d Animation.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/3d%20Animation.jpg.jpeg?updatedAt=1773762683954" 
             },
             { 
                 title: "Canva", cat: "designing", duration: "1–2 Months ", mode: "Offline / Online",
                 software: "Canva (Free & Pro), Brand Kit, Templates, AI Features",
                 desc: "Learn fast, creative, and professional graphic design using Canva — perfect for beginners, marketers, and business owners..", 
                 careers: ["Graphic Designe", "Social Media Designer", "Marketing Executive", "Freelancer", "Content Creator"],
-                img: "img/CANVA.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/CANVA.jpg.jpeg?updatedAt=1773762683991" 
             },
             { 
                 title: "AutoCAD", cat: "designing", duration: "3–6 Months", mode: "Online / Offline",
                 software: "AutoCAD",
                 desc: "Master AutoCAD and learn professional 2D drafting used in architecture, interior design, and engineering industries.", 
                 careers: ["AutoCAD Draftsman", "Civil CAD Designer", "Interior CAD Designer","Site Assistant"],
-                img: "img/AUTOCAD.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/AUTOCAD.jpg.jpeg?updatedAt=1773762683964" 
             },
            
             
@@ -64,70 +64,70 @@
                 software: "HTML5, VS Code",
                 desc: "Learn the foundation of web development by mastering HTML, the building block of all websites.", 
                 careers: ["Web Designer","Front-End Developer","Website Assistant"],
-                img: "img/html.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/html.jpeg?updatedAt=1773762683999" 
             },
             { 
                 title: "CSS3 ", cat: "programming", duration: "1–2 Months", mode: "Offline / Online",
                 software: "CSS3, Bootstrap",
                 desc: "Design beautiful and responsive websites using modern CSS techniques.", 
                 careers: ["Web Designer","Front-End Developer"],
-                img: "img/css.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/css.jpg.jpeg?updatedAt=1773762684054" 
             },
             { 
                 title: "JavaScript", cat: "programming", duration: "2–3 Months", mode: "Offline / Online",
                 software: "JavaScript (ES6), Browser Dev Tools",
                 desc: "Add interactivity and dynamic functionality to websites using JavaScript.", 
                 careers: ["Front-End Developer","Web Developer","JavaScript Developer"],
-                img: "img/js.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/js.jpg.jpeg?updatedAt=1773762684007" 
             },
             { 
                 title: "JQeuery", cat: "programming", duration: "1–2 Months", mode: "Online / Offline",
                 software: " jQuery Library",
                 desc: "Simplify JavaScript coding and create dynamic website effects easily.", 
                 careers: ["Web Developer","Front-End Developer"],
-                img: "img/jquery.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/jquery.jpg.jpeg?updatedAt=1773762684033" 
             },
             { 
                 title: "WordPress", cat: "programming", duration: " 2–4 Months", mode: "Online / Offline",
                 software: "WordPress, Elementor, Plugins & Themes",
                 desc: "Create professional websites without heavy coding using WordPress.", 
                 careers: ["WordPress Developer","Web Designer","Freelance Website Developer"],
-                img: "img/wordpress.png" 
+                img: "https://ik.imagekit.io/motionmax25/courses/wordpress.png?updatedAt=1773762684031" 
             },
             { 
                 title: "Python", cat: "programming", duration: " 4–6 Months", mode: "Offline / Online",
                 software: " Python, VS Code, Basic Django Introduction",
                 desc: "Learn one of the most in-demand programming languages used in web development, data science, and automation", 
                 careers: ["Python Developer","Software Developer","Data Analyst","Automation Engineer"],
-                img: "img/python-.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/python-.jpg.jpeg?updatedAt=1773762684020" 
             },
             { 
                 title: "Web Design", cat: "programming", duration: "6 Months", mode: "Online / Offline",
                 software: "HTML5, CSS3, JavaScript, Bootstrap, WordPress",
                 desc: "Step into the world of modern website creation and learn how to design responsive, user-friendly websites from scratch. This course covers front-end development, design principles, and real-world projects.", 
                 careers: ["Web Designer","Front-End Developer","WordPress Developer","Freelance Web Developer"],
-                img: "img/web design.jpg (1).jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/web%20design.jpg%20(1).jpeg?updatedAt=1773762683994" 
             },
             { 
                 title: "UI / UX", cat: "programming", duration: "6 Months", mode: "Online / Offline",
                 software: "Figma, Adobe XD, Photoshop",
                 desc: "Learn how to design beautiful, user-friendly digital experiences for websites and mobile apps. This course focuses on research, wireframing, prototyping, and usability principles used in real industry projects.", 
                 careers: ["UI Designer","UX Designer","Product Designer","Interaction Designer","App Designer"],
-                img: "img/ui ux.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/ui%20ux.jpg.jpeg?updatedAt=1773762684035" 
             },
             { 
                 title: "AI Tools for Design & Office", cat: "programming", duration: "2–3 Months", mode: "Offline / Online",
                 software: "ChatGPT, Canva AI, AI Image Tools, AI Content Tools",
                 desc: "Learn how to use Artificial Intelligence tools to work smarter, faster, and more creatively in design, content, and office tasks.", 
                 careers: ["AI Content Creator", "Digital Marketer ", "Designer", "Office Automation Executive", "Freelancer"],
-                img: "img/ai tools.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/ai%20tools.jpg.jpeg?updatedAt=1773762684052" 
             },
             { 
                 title: "Hardware & Networking", cat: "programming", duration: "3–6 Months", mode: "Online / Offline",
                 software: "Computer Hardware, Windows OS, Networking Tools",
                 desc: "Build a strong foundation in computer hardware, networking, and troubleshooting for IT support roles.", 
                 careers: ["Hardware Engineer ", "Network Technician", " IT Support Executive", "System Administrator (Junior)"],
-                img: "img/hardware & networking.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/hardware%20&%20networking.jpg.jpeg?updatedAt=1773762683948" 
             },
 
             { 
@@ -135,21 +135,21 @@
                 software: "MS Word, MS Excel, MS PowerPoint, MS Outlook",
                 desc: "Master essential computer skills with Microsoft Office and become job-ready for office, admin, and business roles. This course covers all major Office tools with practical training.", 
                 careers: ["Office Executive","Data Entry Operator","Computer Operator","Back Office Staff"],
-                img: "img/ms office.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/ms%20Office.jpg.jpeg?updatedAt=1773762683997" 
             },
              { 
                 title: "Advanced Excel ", cat: "business", duration: "2–4 Months", mode: "Online / Offline",
                 software: "Microsoft Excel (Advanced Tools & Functions)",
                 desc: "Become an Excel expert and learn advanced tools used in corporate offices for data analysis and reporting.", 
                 careers: ["MIS Executive","Data Analyst","Accounts Executive","Business Analyst"],
-                img: "img/Excel.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/Excel.jpg.jpeg?updatedAt=1773762684044" 
             },
             { 
                 title: "Tally Prime", cat: "business", duration: "3–6 Months", mode: "Online / Offline",
                 software: " Tally Prime",
                 desc: "Learn practical accounting with Tally and become ready for real-world accounting jobs.", 
                 careers: ["Account Assistant","Billing Executive","Tally Operator","Junior Accountant"],
-                img: "img/tally-prime.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/tally-prime.jpg.jpeg?updatedAt=1773762684011" 
             },
 
             { 
@@ -157,14 +157,14 @@
                 software: "Tally Prime (Advanced Features)",
                 desc: "Upgrade your accounting skills with advanced taxation, GST filing, and financial reporting.", 
                 careers: ["Senior Accountant","GST Practitioner","Tax Consultant","Finance Executive"],
-                img: "img/tally +gst.jpg.jpeg" 
+                img: "https://ik.imagekit.io/motionmax25/courses/tally%20+gst.jpg.jpeg?updatedAt=1773762684026" 
             },
              { 
                 title: "Data Analysis", cat: "business", duration: " 4–6 Months ", mode: "Online / Offline",
                 software: "Advanced Excel, Power BI, Basic SQL, Data Visualization Tools",
                 desc: "Learn how to analyze, visualize, and interpret data to support business decisions using industry-relevant tools.", 
                 careers: ["Reporting Analyst","Business Analyst","MIS Executive","Data Analyst"],
-                img: "img/DATA analysis.jpg.jpeg" 
+                img: "" 
             }
         ];
 
